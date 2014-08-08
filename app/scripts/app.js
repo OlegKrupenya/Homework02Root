3d9fbac3300a51003337aee00a5019e89b3ab81e
unknown <taipan90@rambler.ru>
2014-08-08 20:41:42 +0300

    commit of the all files

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  angular.module('auction', ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+          .when('/', {
+              templateUrl: 'views/home.html',
+              controller: 'HomeController',
+              controllerAs: 'ctrl'
+          })
+          .when('/search', {
+              templateUrl: 'views/search.html',
+              controller: 'SearchController',
+              controllerAs: 'search'
+          })
+          .otherwise({
+              redirectTo: '/'
+          });
+  }]);
+}());
